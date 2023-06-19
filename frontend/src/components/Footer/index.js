be29361749da2c40122a9eb568c6264bba8c0466
ARPAN MONDAL <accesstoarpan@gmail.com>
2023-06-19 11:06:25 +0530

    Update index.js

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,10 +6,10 @@ const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.brand}>
       <div className={styles.created}>
-        Created by masaun
+        Created by Arpan Mondal
       </div>
       <div className={styles.copyright}>
-        Copyright © masaun
+        Copyright © Arpan Mondal
       </div>
     </div>
   </footer>
